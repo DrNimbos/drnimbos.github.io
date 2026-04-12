053ed499a741f03790a8af25659ac0d83e1f688a
DrNimbos <guilhermelima.meneses@gmail.com>
2026-04-12 11:21:01 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.querySelector("#btn1").addEventListener("click", function(){
+    window.location.href = "pages/gamedev/abominatio.html";
+})
+document.querySelector("#btn2").addEventListener("click", function(){
+    window.location.href = "gamedev.html";
+})
+document.querySelector("#btn3").addEventListener("click", function(){
+    window.location.href = "gamedev.html";
+})
+
